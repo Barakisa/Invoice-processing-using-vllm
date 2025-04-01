e0211cbe82453d1ba2383c46fe5564c8ab4a1cbf
Barakisa <nojus.dziaugys@gmail.com>
2025-04-01 10:16:52 +0300

    merge server with laptop version

---
+++ qwen_server_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+vllm serve Qwen/Qwen2-VL-7B-Instruct \
+  --task generate \
+  --trust_remote_code \
+  --host 192.168.1.220 \
+  --port 8000 \
+  --dtype=half \
+  --gpu_memory_utilization=1 \
+  --max-num-seqs 2 \
+  --max-model-len 16384 \
+  --limit-mm-per-prompt image=4,video=0 \
+  --chat_template ./template_qwen.jinja \
+  --no-enable-prefix-caching
+
